a388a88f24e19e2f3e0258c88a15b8325617b3d7
obakyan <obakyan@gmail.com>
2019-05-26 23:32:39 +0900

    add upperbound

--- sublua/bound.lua
+++ sublua/bound.lua
@@ -14,3 +14,19 @@ local function lower_bound(ary, x)
   end
   return max
 end
+
+local function upper_bound(ary, x)
+  if(x < ary[1]) then return 1 end
+  local num = #ary
+  if(ary[num] <= x) then return num + 1 end
+  local min, max = 1, num
+  while(1 < max - min) do
+    local mid = mfl((min + max) / 2)
+    if(ary[mid] <= x) then
+      min = mid
+    else
+      max = mid
+    end
+  end
+  return max
+end
