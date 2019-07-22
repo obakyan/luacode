f35d1b061af3b1be8efb59b4e951831f51eac758
obakyan <obakyan@gmail.com>
2019-07-22 13:11:40 +0900

    update union-find

--- sublua/kruskal.lua
+++ sublua/kruskal.lua
@@ -5,8 +5,12 @@ local function uf_initialize(n)
 end
 
 local function uf_findroot(idx, parent)
+  local idx_update = idx
   while parent[idx] ~= idx do
     idx = parent[idx]
+  end
+  while parent[idx_update] ~= idx do
+    parent[idx_update], idx_update = idx, parent[idx_update]
   end
   return idx
 end
