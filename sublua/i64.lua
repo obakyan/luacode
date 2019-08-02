55212a820f530ff8f55d3c0af7e9949f2ce071b7
obakyan <obakyan@gmail.com>
2019-08-02 11:23:49 +0900

    update

--- sublua/i64.lua
+++ sublua/i64.lua
@@ -1,13 +1,18 @@
 local mfl = math.floor
 
-local i64 = {v = {0, 0}, c = 1000000000}
+local i64 = {v = {0, 0}, c = 10000000000}
 i64.inc = function(self, x)
   assert(0 <= x, "use dec")
   self.v[2] = self.v[2] + x
-  self.v[1], self.v[2] = self.v[1] + mfl(self.v[2] / big), self.v[2] % big
+  self.v[1], self.v[2] = self.v[1] + mfl(self.v[2] / self.c), self.v[2] % self.c
 end
 i64.dump = function(self)
-  local s2 = 0 < self.v[1] and string.format("%09d", self.v[2]) or self.v[2]
+  local s2 = 0 < self.v[1] and string.format("%010d", self.v[2]) or self.v[2]
   local s1 = 0 < self.v[1] and self.v[1] or ""
   return s1 .. s2
+end
+i64.create = function()
+  local obj = {}
+  setmetatable(obj, {__index = i64})
+  return obj
 end
