fd447f99e84eceafdf84de14fefc748a61f1247d
obakyan <obakyan@gmail.com>
2019-08-05 23:04:55 +0900

    update bound

--- sublua/bound.lua
+++ sublua/bound.lua
@@ -1,13 +1,18 @@
 local mfl = math.floor
+
+local function comp(a, b)
+  return a < b
+end
+
 local function lower_bound(ary, x)
   local num = #ary
   if num == 0 then return 1 end
-  if x <= ary[1] then return 1 end
-  if ary[num] < x then return num + 1 end
+  if not comp(ary[1], x) then return 1 end
+  if comp(ary[num], x) then return num + 1 end
   local min, max = 1, num
   while 1 < max - min do
     local mid = mfl((min + max) / 2)
-    if ary[mid] < x then
+    if comp(ary[mid], x) then
       min = mid
     else
       max = mid
@@ -19,12 +24,12 @@ end
 local function upper_bound(ary, x)
   local num = #ary
   if num == 0 then return 1 end
-  if x < ary[1] then return 1 end
-  if ary[num] <= x then return num + 1 end
+  if comp(x, ary[1]) then return 1 end
+  if not comp(x, ary[num]) then return num + 1 end
   local min, max = 1, num
   while 1 < max - min do
     local mid = mfl((min + max) / 2)
-    if ary[mid] <= x then
+    if not comp(x, ary[mid]) then
       min = mid
     else
       max = mid
