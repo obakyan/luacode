ec22ae9f6676afb663d50fe6b329a15107c9acb0
obakyan <obakyan@gmail.com>
2019-09-10 14:20:19 +0900

    improve seg-avl

--- sublua/seg_avltree.lua
+++ sublua/seg_avltree.lua
@@ -4,7 +4,7 @@ local SegForAvl = {}
 SegForAvl.updateAll = function(self)
   for i = self.stagenum - 1, 1, -1 do
     for j = 1, self.cnt[i] do
-      self.stage[i][j] = self.stage[i + 1][j * 2 - 1]
+      self.stage[i][j] = self.stage[i + 1][j * 2]
     end
   end
 end
@@ -17,7 +17,8 @@ SegForAvl.create = function(self, n)
   end
   for i = 1, stagenum do self.size[i] = self.cnt[stagenum + 1 - i] end
   self.stagenum = stagenum
-  for i = 1, mul do self.stage[stagenum][i] = i end
+  self.stage[stagenum][1] = false
+  for i = 2, mul do self.stage[stagenum][i] = i end
   self:updateAll()
 end
 SegForAvl.hold = function(self)
@@ -51,33 +52,34 @@ end
 local SegAvlTree = {}
 SegAvlTree.makenode = function(self, val, parent)
   local i = self.seg:hold()
-  self.v[i], self.u[i], self.p[i] = val, true, parent
-  self.lc[i], self.rc[i], self.l[i], self.r[i] = 0, 0, 0, 0
+  self.v[i], self.p[i] = val, parent
+  self.lc[i], self.rc[i], self.l[i], self.r[i] = 0, 0, 1, 1
   return i
 end
 SegAvlTree.create = function(self, lessthan, n)
   self.lessthan = lessthan
-  self.seg = SegForAvl.new(n)
-  -- value, leftCount, rightCount, left, right, parent, used
-  self.v, self.lc, self.rc, self.l, self.r, self.p, self.u = {}, {}, {}, {}, {}, {}, {}
-  for i = 1, n do
-    self.v[i], self.u[i], self.p[i] = 0, false, 0
-    self.lc[i], self.rc[i], self.l[i], self.r[i] = 0, 0, 0, 0
+  self.root = 1
+  self.seg = SegForAvl.new(n + 1)
+  -- value, leftCount, rightCount, left, right, parent
+  self.v, self.lc, self.rc, self.l, self.r, self.p = {}, {}, {}, {}, {}, {}
+  for i = 1, n + 1 do
+    self.v[i], self.p[i] = 0, 1
+    self.lc[i], self.rc[i], self.l[i], self.r[i] = 0, 0, 1, 1
   end
 end
 
 SegAvlTree.recalcCount = function(self, i)
-  if self.u[i] then
-    if self.u[self.l[i]] then self.lc[i] = 1 + mma(self.lc[self.l[i]], self.rc[self.l[i]])
+  if 1 < i then
+    if 1 < self.l[i] then self.lc[i] = 1 + mma(self.lc[self.l[i]], self.rc[self.l[i]])
     else self.lc[i] = 0
     end
-    if self.u[self.r[i]] then self.rc[i] = 1 + mma(self.lc[self.r[i]], self.rc[self.r[i]])
+    if 1 < self.r[i] then self.rc[i] = 1 + mma(self.lc[self.r[i]], self.rc[self.r[i]])
     else self.rc[i] = 0
     end
   end
 end
 SegAvlTree.recalcCountAll = function(self, i)
-  while self.u[i] do
+  while 1 < i do
     self:recalcCount(i)
     i = self.p[i]
   end
@@ -87,8 +89,8 @@ SegAvlTree.rotR = function(self, child, parent)
   local granp = self.p[parent]
   self.r[child], self.l[parent] = parent, self.r[child]
   self.p[child], self.p[parent] = granp, child
-  if self.u[self.l[parent]] then self.p[self.l[parent]] = parent end
-  if self.u[granp] then
+  self.p[self.l[parent]] = parent
+  if 1 < granp then
     if self.l[granp] == parent then
       self.l[granp] = child
     else
@@ -104,8 +106,8 @@ SegAvlTree.rotL = function(self, child, parent)
   local granp = self.p[parent]
   self.l[child], self.r[parent] = parent, self.l[child]
   self.p[child], self.p[parent] = granp, child
-  if self.u[self.r[parent]] then self.p[self.r[parent]] = parent end
-  if self.u[granp] then
+  self.p[self.r[parent]] = parent
+  if 1 < granp then
     if self.r[granp] == parent then
       self.r[granp] = child
     else
@@ -118,12 +120,12 @@ SegAvlTree.rotL = function(self, child, parent)
 end
 
 SegAvlTree.add = function(self, val)
-  if not self.u[self.root] then self.root = self:makenode(val, 0) return end
+  if self.root <= 1 then self.root = self:makenode(val, 1) return end
   local pos = self.root
   local added = false
   while not added do
     if self.lessthan(val, self.v[pos]) then
-      if self.u[self.l[pos]] then
+      if 1 < self.l[pos] then
         pos = self.l[pos]
       else
         self.l[pos] = self:makenode(val, pos)
@@ -131,7 +133,7 @@ SegAvlTree.add = function(self, val)
         added = true
       end
     else
-      if self.u[self.r[pos]] then
+      if 1 < self.r[pos] then
         pos = self.r[pos]
       else
         self.r[pos] = self:makenode(val, pos)
@@ -140,9 +142,9 @@ SegAvlTree.add = function(self, val)
       end
     end
   end
-  while self.u[pos] do
+  while 1 < pos do
     local child, parent = pos, self.p[pos]
-    if not self.u[parent] then
+    if parent <= 1 then
       break
     end
     self:recalcCount(parent)
@@ -158,10 +160,10 @@ SegAvlTree.add = function(self, val)
           self:rotL(cr, child)
           self:rotR(cr, parent)
         end
-        pos = nil
+        pos = 1
       else
         self:recalcCountAll(child)
-        pos = nil
+        pos = 1
       end
     else -- parent.r == child
       if self.rc[parent] - 1 == self.lc[parent] then
@@ -175,27 +177,27 @@ SegAvlTree.add = function(self, val)
           self:rotR(cl, child)
           self:rotL(cl, parent)
         end
-        pos = nil
+        pos = 1
       else
         self:recalcCountAll(child)
-        pos = nil
+        pos = 1
       end
     end
   end
 end
 
 SegAvlTree.rmsub = function(self, node)
-  while self.u[node] do
+  while 1 < node do
     self:recalcCount(node)
     if self.lc[node] == self.rc[node] then
       node = self.p[node]
     elseif self.lc[node] + 1 == self.rc[node] then
       self:recalcCountAll(self.p[node])
-      node = nil
+      node = 1
     else
       if self.lc[self.r[node]] == self.rc[self.r[node]] then
         self:rotL(self.r[node], node)
-        node = nil
+        node = 1
       elseif self.lc[self.r[node]] + 1 == self.rc[self.r[node]] then
         local nr = self.r[node]
         self:rotL(nr, node)
@@ -212,16 +214,16 @@ end
 
 SegAvlTree.pop = function(self)
   local node = self.root
-  while self.u[self.l[node]] do
+  while 1 < self.l[node] do
     node = self.l[node]
   end
   local v = self.v[node]
-  if self.u[self.p[node]] then
-    if self.u[self.r[node]] then self.p[self.r[node]] = self.p[node] end
+  if 1 < self.p[node] then
+    self.p[self.r[node]] = self.p[node]
     self.l[self.p[node]] = self.r[node]
     self:rmsub(self.p[node])
   else
-    if self.u[self.r[node]] then self.p[self.r[node]] = 0 end
+    self.p[self.r[node]] = 1
     self.root = self.r[node]
   end
   self.seg:release(node)
