1134929af6ea477d13484c9e85b783b448f85d89
unknown <IshimatsuHiroyuki@ishimatsuPC.cric.co.jp>
2019-11-14 09:27:18 +0900

    update segbound. remove vector

--- sublua/subseg/segbound.lua
+++ sublua/subseg/segbound.lua
@@ -29,10 +29,8 @@ end
 -- if not exist then return right + 1
 SegTree.right_bound = function(self, val, left, right)
   local ret, retpos = self.emptyvalue, left - 1
-  local t1, t2, t3 = {1}, {left}, {right}
-  while 0 < #t1 do
-    local stage, l, r = t1[#t1], t2[#t1], t3[#t1]
-    table.remove(t1) table.remove(t2) table.remove(t3)
+  local stage, l, r = 1, left, right
+  while true do
     local sz = self.size[stage]
     while (l - 1) % sz ~= 0 or r + 1 - l < sz do
       stage = stage + 1
@@ -42,9 +40,11 @@ SegTree.right_bound = function(self, val, left, right)
     if tmp < val then
       ret, retpos = tmp, l + sz - 1
       if retpos == right then break end
-      if l + sz <= r then table.insert(t1, 1) table.insert(t2, l + sz) table.insert(t3, r) end
+      if l + sz <= r then stage, l, r = 1, l + sz, r
+      else break end
     else
-      if sz ~= 1 then table.insert(t1, stage + 1) table.insert(t2, l) table.insert(t3, l + sz - 2) end
+      if sz ~= 1 then stage, l, r = stage + 1, l, l + sz - 2
+      else break end
     end
   end
   return retpos + 1
@@ -56,10 +56,8 @@ end
 -- if not exist then return left - 1
 SegTree.left_bound = function(self, val, left, right)
   local ret, retpos = self.emptyvalue, right + 1
-  local t1, t2, t3 = {1}, {left}, {right}
-  while 0 < #t1 do
-    local stage, l, r = t1[#t1], t2[#t1], t3[#t1]
-    table.remove(t1) table.remove(t2) table.remove(t3)
+  local stage, l, r = 1, left, right
+  while true do
     local sz = self.size[stage]
     while r % sz ~= 0 or r + 1 - l < sz do
       stage = stage + 1
@@ -68,9 +66,11 @@ SegTree.left_bound = function(self, val, left, right)
     local tmp = self.func(ret, self.stage[stage][mfl(r / sz)])
     if tmp < val then
       ret, retpos = tmp, r - sz + 1
-      if l + sz <= r then table.insert(t1, 1) table.insert(t2, l) table.insert(t3, r - sz) end
+      if l + sz <= r then stage, l, r = 1, l, r - sz
+      else break end
     else
-      if sz ~= 1 then table.insert(t1, stage + 1) table.insert(t2, r - sz + 2) table.insert(t3, r) end
+      if sz ~= 1 then stage, l, r = stage + 1, r - sz + 2, r
+      else break end
     end
   end
   return retpos - 1
