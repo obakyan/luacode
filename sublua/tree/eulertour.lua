4e04cda3fb7bc5ff552bea7c30a65861fda20708
obakyan <obakyan@gmail.com>
2019-12-29 19:48:18 +0900

    improve eulertour (avoid to delete edges)

--- sublua/tree/eulertour.lua
+++ sublua/tree/eulertour.lua
@@ -1,10 +1,12 @@
 local n = io.read("*n")
 local edge = {}
 local asked = {}
+local cpos = {}
 local len = {}
 for i = 1, n do
   edge[i] = {}
   asked[i] = false
+  cpos[i] = 0
   len[i] = 0
 end
 for i = 1, n - 1 do
@@ -19,16 +21,14 @@ while 0 < #tasks do
   table.remove(tasks)
   asked[src] = true
   -- print(src, len[src])
-  while 0 < #edge[src] do
-    local dst = edge[src][#edge[src]]
+  while cpos[src] < #edge[src] do
+    cpos[src] = cpos[src] + 1
+    local dst = edge[src][cpos[src]]
     if not asked[dst] then
       len[dst] = len[src] + 1
       table.insert(tasks, src)
       table.insert(tasks, dst)
-      table.remove(edge[src])
       break
-    else
-      table.remove(edge[src])
     end
   end
 end
