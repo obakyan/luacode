00081ac1b1808cd5e37a763a0e94696efbbaa037
obakyan <obakyan@gmail.com>
2020-06-12 00:13:03 +0900

    [TBD] update BFS of MPM. calc send-reveive amount

--- sublua/mpm.lua
+++ sublua/mpm.lua
@@ -8,17 +8,19 @@ MPM.initialize = function(self, n, spos, tpos)
   self.edge_dst_invidx = {}
   self.len = {}
   self.sub_graph_flag = {}
+  self.send = {}
+  self.receive = {}
   for i = 1, n do
     self.edge_dst[i] = {}
     self.edge_cap[i] = {}
     self.edge_dst_invidx[i] = {}
     self.len[i] = 0
     self.sub_graph_flag[i] = false
+    self.send[i] = 0
+    self.receive[i] = 0
   end
   self.sub_graph_v = {}
   self.sub_graph_size = 0
-  self.send = {}
-  self.receive = {}
 end
 
 MPM.addEdge = function(self, src, dst, cap)
@@ -33,10 +35,12 @@ MPM.makeSubGraph = function(self)
   local inf = self.n + 1
   local len, sub_graph_flag = self.len, self.sub_graph_flag
   local edge_dst, edge_cap = self.edge_dst, self.edge_cap
+  local send, receive = self.send, self.receive
   local sub_graph_v = self.sub_graph_v
   for i = 1, self.n do
     len[i] = inf
     sub_graph_flag[i] = false
+    send[i], receive[i] = 0, 0
   end
   -- BFS
   len[self.spos] = 0
@@ -46,14 +50,20 @@ MPM.makeSubGraph = function(self)
   while done < taskcnt do
     done = done + 1
     local src = sub_graph_v[done]
+    if src == self.tpos then reached = true break end
     for i = 1, #edge_dst[src] do
-      if 0 < edge_cap[src][i] then
+      local cap = edge_cap[src][i]
+      if 0 < cap then
         local dst = edge_dst[src][i]
         if len[dst] == inf then
+          send[src] = send[src] + cap
+          receive[dst] = receive[dst] + cap
           len[dst] = len[src] + 1
           taskcnt = taskcnt + 1
           sub_graph_v[taskcnt] = dst
-          if dst == self.tpos then reached = true break end
+        elseif len[dst] == len[src] + 1 then
+          send[src] = send[src] + cap
+          receive[dst] = receive[dst] + cap
         end
       end
     end
@@ -78,6 +88,16 @@ MPM.makeSubGraph = function(self)
         sub_graph_flag[src] = true break
       end
     end
+    if not sub_graph_flag[src] then
+      for i = 1, #edge_dst[src] do
+        local dst = edge_dst[src][i]
+        local cap = edge_cap[src][i]
+        if 0 < cap and len[dst] == len[src] + 1 then
+          send[src] = send[src] - cap
+          receive[dst] = receive[dst] - cap
+        end
+      end
+    end
   end
   -- remove unused vertex
   local nodecnt = 1
@@ -98,14 +118,6 @@ MPM.flow = function(self)
   local edge_dst, edge_cap = self.edge_dst, self.edge_cap
   local sub_graph_v = self.sub_graph_v
   local gsize = self.sub_graph_size
-  for i = 1, gsize do
-    self.send[i] = 0
-    self.receive[i] = 0
-  end
-  -- update send-receive amount
-  for i = 1, gsize do
-    local v = sub_graph_v[i]
-  end
 end
 
 MPM:initialize(7, 1, 7)
