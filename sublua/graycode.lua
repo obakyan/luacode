f073f228569ade1e97b5c28b95378d8267d83f9b
obakyan <obakyan@gmail.com>
2020-07-14 10:54:50 +0900

    improve graycode O(logN) to O(1)

--- sublua/graycode.lua
+++ sublua/graycode.lua
@@ -1,18 +1,6 @@
-local mfl = math.floor
-local function grayCode(n)
-  local ret = {}
-  local tot = 1
-  for i = 1, n do tot = tot * 2 end
-  for i = 1, tot - 1 do
-    local v, m, prv = 0, 1, i % 2
-    i = mfl(i / 2)
-    for j = 1, n - 1 do
-      local cur = i % 2
-      if prv + cur == 1 then v = v + m end
-      prv, m, i = cur, m * 2, mfl(i / 2)
-    end
-    if prv == 1 then v = v + m end
-    table.insert(ret, v)
-  end
-  return ret
+local bls, brs = bit.lshift, bit.rshift
+local bxor = bit.bxor
+
+local function grayCode(x)
+  return bxor(x, brs(x, 1))
 end
