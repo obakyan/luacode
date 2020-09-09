78d19c1bb363bf77a603bcf0ba4ee5d318289a34
obakyan <obakyan@gmail.com>
2020-09-09 21:07:38 +0900

    add toposort to SCCD

--- sublua/sccd.lua
+++ sublua/sccd.lua
@@ -61,7 +61,7 @@ local function SCCD_invdfs(spos, rootid)
   end
 end
 
-local function SCCD_make()
+local function SCCD_categorize()
   for src = 1, n do
     if not asked[src] then
       local dfs_way = {}
@@ -76,31 +76,62 @@ local function SCCD_make()
   end
 end
 
-SCCD_make()
-
 local gsize = {}
 local gedge, ginvedge = {}, {}
 local glen = {}
 local gmember = {}
-for i = 1, n do
-  gsize[i] = 0
-  gedge[i], ginvedge[i] = {}, {}
-  glen[i] = 0
-  gmember[i] = {}
-end
-for i = 1, n do
-  local r = sccd_root[i]
-  gsize[r] = gsize[r] + 1
-  table.insert(gmember[r], i)
-end
-for i = 1, m do
-  local a, b = edgeinfo[i][1], edgeinfo[i][2]
-  local ra, rb = sccd_root[a], sccd_root[b]
-  if ra ~= rb then
-    table.insert(gedge[ra], rb)
-    table.insert(ginvedge[rb], ra)
+
+local function SCCD_make_group_graph()
+  for i = 1, n do
+    gsize[i] = 0
+    gedge[i], ginvedge[i] = {}, {}
+    glen[i] = 0
+    gmember[i] = {}
+  end
+  for i = 1, n do
+    local r = sccd_root[i]
+    gsize[r] = gsize[r] + 1
+    table.insert(gmember[r], i)
+  end
+  for i = 1, m do
+    local a, b = edgeinfo[i][1], edgeinfo[i][2]
+    local ra, rb = sccd_root[a], sccd_root[b]
+    if ra ~= rb then
+      table.insert(gedge[ra], rb)
+      table.insert(ginvedge[rb], ra)
+    end
   end
 end
+
+local function SCCD_toposort()
+  local topo_tasks = {}
+  local topo_asked_cnt = {}
+  for i = 1, n do
+    topo_asked_cnt[i] = 0
+    if 0 < gsize[i] and #ginvedge[i] == 0 then
+      table.insert(topo_tasks, i)
+    end
+  end
+  local topoary = {}
+  local topo_done = 0
+  while topo_done < #topo_tasks do
+    topo_done = topo_done + 1
+    local g = topo_tasks[topo_done]
+    topoary[topo_done] = g
+    for i = 1, #gedge[g] do
+      local dst = gedge[g][i]
+      topo_asked_cnt[dst] = topo_asked_cnt[dst] + 1
+      if topo_asked_cnt[dst] == #ginvedge[dst] then
+        table.insert(topo_tasks, dst)
+      end
+    end
+  end
+  return topoary
+end
+
+SCCD_categorize()
+SCCD_make_group_graph()
+
 for i = 1, n do
   if 0 < gsize[i] then
     print(i .. " : " .. table.concat(gmember[i], ", "))
@@ -112,3 +143,6 @@ for i = 1, n do
     end
   end
 end
+
+local topoary = SCCD_toposort()
+print(table.concat(topoary, " "))
